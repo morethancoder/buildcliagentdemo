a349ded14e0109f4b0e400f2146941169724a17a
morethancoder <aalili.234.alico@gmail.com>
2025-09-25 09:16:01 +0300

    interactive cli ai agent with colors

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/openai/openai-go/v2"
 	"github.com/openai/openai-go/v2/option"
 )
@@ -38,8 +39,12 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
+	fmt.Println(color.CyanString(
+		fmt.Sprintf("CLI Agent powered by (%s)", model),
+	))
+
 	for {
-		fmt.Print("> ")
+		fmt.Print(color.GreenString("\n> "))
 
 		if !scanner.Scan() {
 			break
@@ -50,14 +55,38 @@ func main() {
 			continue
 		}
 
+		if input == "clear" {
+			fmt.Print("\033[H\033[2J")
+			continue
+		}
+
+		if input == "exit" {
+			fmt.Println(color.WhiteString("see ya"))
+			break
+		}
+
 		params.Messages = append(params.Messages, openai.UserMessage(input))
 
 		res, err := client.Chat.Completions.New(ctx, params)
 		if err != nil {
-			log.Println(err)
+			log.Println(color.RedString(err.Error()))
 		}
 
-		fmt.Println(res.Choices[0].Message.Content)
+		output := res.Choices[0].Message.Content
+		fmt.Println("\n", color.YellowString(output))
+
+		params.Messages = append(params.Messages, openai.AssistantMessage(output))
+
+		messagesCount := len(params.Messages)
+		fmt.Println(color.WhiteString(
+			fmt.Sprintf("\nmessages count: %d", messagesCount),
+		))
+
+		if messagesCount > 10 {
+			params.Messages = params.Messages[messagesCount - 4:]
+			//clean up the view
+			fmt.Println(color.WhiteString("cleaned up chat history!"))
+		} 
 
 	}
 
